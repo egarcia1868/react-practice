ce334433e5c3107227fcc7c7ba12952c60636ac0
Eric Garcia <egarcia1868@gmail.com>
2021-01-15 11:59:38 -0600

    react practice 20-10 useContext starting

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -2,16 +2,10 @@ import React from "react";
 import Card from "../Card";
 import "./style.css";
 
-function CardContainer(props) {
+function CardContainer() {
   return (
     <div className="jumbotron card-container">
-      {/* Pass props to Card */}
-      <Card 
-        title={props.title}
-        profileUrl={props.profileUrl}
-        image={props.image}
-        handleBtnClick={props.handleBtnClick}
-      />
+      <Card />
     </div>
   );
 }
--- src/components/CardTitleText/index.js
+++ src/components/CardTitleText/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import CardContext from "../../utils/CardContext";
 
-function CardTitleText(props) {
-  return <h2>{props.title}</h2>;
+function CardTitleText() {
+  return <CardContext.Consumer>{({ title }) => <h2>{title}</h2>}</CardContext.Consumer>;
 }
 
 export default CardTitleText;
